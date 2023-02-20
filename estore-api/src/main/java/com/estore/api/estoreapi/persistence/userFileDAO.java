eed07c9c92a8b01fbd32d69b7ea1ca358872931b
Brendan Battisti <brendan.battisti@gmail.com>
2023-02-19 22:09:22 -0500

    Stubbed out functions

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/userFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/userFileDAO.java
@@ -1,12 +1,12 @@
 package com.estore.api.estoreapi.persistence;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
+import com.estore.api.estoreapi.users.Customer;
 import com.estore.api.estoreapi.users.User;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,33 +14,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
-
 public class UserFileDAO implements UserDAO {
 
-    Map<String, User> users;   // Provides a local cache of the inventory objects
-                             // so that we don't need to read from the file
-                             // each time
-    private ObjectMapper objectMapper;  // Provides conversion between Beef
-                                        // objects and JSON text format written
-                                        // to the file
-    private String filename;    // Filename to read from and write to
+  Map<String, User> users; // Provides a local cache of the inventory objects
+  // so that we don't need to read from the file
+  // each time
+  private ObjectMapper objectMapper; // Provides conversion between user
+                                     // objects and JSON text format written
+                                     // to the file
+  private String filename; // Filename to read from and write to
 
+  public UserFileDAO(@Value("${user.file}") String filename, ObjectMapper objectMapper) throws IOException {
+    this.filename = filename;
+    this.objectMapper = objectMapper;
+    load(); // load the users from the file
+  }
 
-    public UserFileDAO(@Value("${user.file}") String filename, ObjectMapper objectMapper) throws IOException {
-        this.filename = filename;
-        this.objectMapper = objectMapper;
-        load();  // load the users from the file
-    }
-
-    
-    /**
-  * Saves the {@linkplain User user} from the map into the file as an array of JSON objects
-  * 
-  * @return true if the {@link User user} were written successfully
-  * 
-  * @throws IOException when file cannot be accessed or written to
-  */
+  /**
+   * Saves the {@linkplain User user} from the map into the file as an array of
+   * JSON objects
+   * 
+   * @return true if the {@link User user} were written successfully
+   * 
+   * @throws IOException when file cannot be accessed or written to
+   */
   private boolean save() throws IOException {
     User[] userArray = getUserArray();
 
@@ -51,49 +48,96 @@ public class UserFileDAO implements UserDAO {
     return true;
   }
 
-
   /**
-  * Loads {@linkplain User user} from the JSON file into the map
-  * 
-  * @return true if the file was read successfully
-  * 
-  * @throws IOException when file cannot be accessed or read from
-  */
+   * Loads {@linkplain User user} from the JSON file into the map
+   * 
+   * @return true if the file was read successfully
+   * 
+   * @throws IOException when file cannot be accessed or read from
+   */
   private boolean load() throws IOException {
     users = new TreeMap<>();
 
-    // Deserializes the JSON objects from the file into an array of beefInventory
+    // Deserializes the JSON objects from the file into an array of userArray
     // readValue will throw an IOException if there's an issue with the file
     // or reading from the file
-    User[] userArray = objectMapper.readValue(new File(filename),User[].class);
+    User[] userArray = objectMapper.readValue(new File(filename), User[].class);
 
     // Adds each user to the map
-    for (User user : userArray){
+    for (User user : userArray) {
       users.put(user.getUsername(), user);
     }
     return true;
   }
 
   /**
-  * Generates an array of {@linkplain User user} from the tree map for any
-  * {@linkplain User user} that contains the text specified by containsText
-  * <br>
-  * If containsText is null, the array contains all of the {@linkplain User user}
-  * in the tree map
-  * 
-  * @return  The array of {@link Beef beef}, may be empty
-  */
+   * Generates an array of {@linkplain User user} from the tree map for any
+   * {@linkplain User user} that contains the text specified by containsText
+   * <br>
+   * If containsText is null, the array contains all of the {@linkplain User user}
+   * in the tree map
+   * 
+   * @return The array of {@link User user}, may be empty
+   */
   private User[] getUserArray() {
     ArrayList<User> userArrayList = new ArrayList<>();
 
-    for (User user : users.values()){
-        userArrayList.add(user);
-      
+    for (User user : users.values()) {
+      userArrayList.add(user);
+
     }
 
     User[] userArray = new User[userArrayList.size()];
     userArrayList.toArray(userArray);
     return userArray;
   }
-  
+
+  @Override
+  public User CreateUser(User user) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public User GetUser(String username) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public User[] GetUsers() throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean DeleteUser(String username) throws IOException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean IsAdmin(String username) throws IOException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean Checkout(String username) throws IOException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Customer AddToCart(String username) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Customer ClearCart(String username) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
 }
