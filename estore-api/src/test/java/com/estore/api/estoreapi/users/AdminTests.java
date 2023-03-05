42e8a132a6a933a9bd58dcc63c36bfe69d94bcbb
Brendan Battisti <brendan.battisti@gmail.com>
2023-03-05 14:39:17 -0500

    Added tests for Admin and Customer

--- estore-api/src/test/java/com/estore/api/estoreapi/users/AdminTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/users/AdminTests.java
@@ -1,9 +1,34 @@
 package com.estore.api.estoreapi.users;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AdminTests {
 
+    @Test
+    public void testCtor() {
+
+        Admin testAdmin = new Admin("Test");
+
+        assertNotNull(testAdmin, () -> "The admin should not be null");
+    }
+
+    @Test
+    public void testIsAdmin() {
+        Admin testAdmin = new Admin("Test");
+
+        assertTrue(testAdmin.isAdmin(), "The admin should be an admin");
+    }
+
+    @Test
+    public void testGetUsername() {
+        Admin testAdmin = new Admin("Test");
+
+        assertEquals("Test", testAdmin.getUsername());
+    }
 }
--- estore-api/src/test/java/com/estore/api/estoreapi/users/CustomerTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/users/CustomerTests.java
@@ -1,9 +1,37 @@
 package com.estore.api.estoreapi.users;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.estore.api.estoreapi.products.CartBeef;
+
 @SpringBootTest
 public class CustomerTests {
+
+    @Test
+    public void testCtor() {
+
+        Customer testCustomer = new Customer("Test", new CartBeef[0]);
+
+        assertNotNull(testCustomer, () -> "The customer should not be null");
+    }
+
+    @Test
+    public void testIsAdmin() {
+        Customer testCustomer = new Customer("Test", new CartBeef[0]);
+
+        assertFalse(testCustomer.isAdmin(), "The customer should not be an admin");
+    }
+
+    @Test
+    public void testGetUsername() {
+        Customer testCustomer = new Customer("Test", new CartBeef[0]);
+
+        assertEquals("Test", testCustomer.getUsername());
+    }
 
 }
