8c96e5abe06371df946556d9af7e4cb73119a688
Brendan Battisti <brendan.battisti@gmail.com>
2023-03-05 17:45:29 -0500

    Tagged the test tiers and added documentation

--- estore-api/src/test/java/com/estore/api/estoreapi/users/AdminTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/users/AdminTests.java
@@ -4,10 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Tag("Model-tier")
 public class AdminTests {
 
     @Test
