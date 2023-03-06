4e7fed53cbd33efb5de4a177d893999007c3669d
ajc2732 <ajc2732@rit.edu>
2023-03-06 12:54:25 -0500

    added test for CartBeef constructor

--- estore-api/src/test/java/com/estore/api/estoreapi/products/CartBeefTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/products/CartBeefTests.java
@@ -1,9 +1,25 @@
 package com.estore.api.estoreapi.products;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("Model-tier")
 @SpringBootTest
 public class CartBeefTests {
+    
+    @Test
+    public void testCtor(){
+        int expectedId = 2;
+        float expectedWeight = (float)10.1;
+
+        CartBeef testCartBeef = new CartBeef(expectedId, expectedWeight);
+
+        assertEquals(expectedId, testCartBeef.getId());
+        assertEquals(expectedWeight, testCartBeef.getWeight());
+    }
+
 
 }
