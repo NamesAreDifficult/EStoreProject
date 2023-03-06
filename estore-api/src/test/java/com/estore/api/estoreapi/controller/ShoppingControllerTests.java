a741e7606ee17ceaff07b50a88b5efe89375874f
ajc2732 <ajc2732@rit.edu>
2023-03-06 12:07:41 -0500

    Began to write tests for ShoppingController File

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingControllerTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingControllerTests.java
@@ -1,9 +1,18 @@
 package com.estore.api.estoreapi.controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Tag;
+
+import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+import com.estore.api.estoreapi.persistence.UserDAO;
+
+@Tag("Controller-tier")
 public class ShoppingControllerTests {
+    private ShoppingController shoppingController;
+    private UserDAO mockUserDAO;
+
+
 
 }
