9b0460ca86213be491fe844ac21470c7dbfeebbc
Groovymeister <jrw2936@rit.edu>
2023-03-06 10:32:17 -0500

    Stubbed basic methods to be testing

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/UserFileDAOTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/UserFileDAOTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +13,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -23,4 +25,25 @@ public class UserFileDAOTests {
     UserFileDAO userFileDAO;
     User[] testUsers;
     ObjectMapper mockObjectMapper;
+
+    @BeforeEach
+    public void setupUserFileDao() throws IOException{}
+
+    @Test
+    public void testGetUserArray(){}
+
+    @Test
+    public void testGetCustomer(){}
+
+    @Test
+    public void testGetUser(){}
+
+    @Test
+    public void testGetUsers(){}
+
+    @Test
+    public void testDeleteUser(){}
+
+    @Test
+    public void testIsAdmin(){}
 }
