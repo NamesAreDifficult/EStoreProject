48241c2e813101f818e69b7e35227909fddfd141
Groovymeister <jrw2936@rit.edu>
2023-03-06 10:20:39 -0500

    Initial commit

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/UserFileDAOTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/UserFileDAOTests.java
@@ -1,9 +1,26 @@
 package com.estore.api.estoreapi.persistence;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.estore.api.estoreapi.users.User;
+
+@Tag("Persistence-tier")
 public class UserFileDAOTests {
-
+    UserFileDAO userFileDAO;
+    User[] testUsers;
+    ObjectMapper mockObjectMapper;
 }
