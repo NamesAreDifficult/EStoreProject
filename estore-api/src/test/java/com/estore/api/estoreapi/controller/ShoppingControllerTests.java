2ef4ee9ec88ec04ebcab246a53d902ef9e01a2a5
ajc2732 <ajc2732@rit.edu>
2023-03-06 17:31:58 -0500

    stubbed out the tests for the file

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingControllerTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingControllerTests.java
@@ -2,15 +2,21 @@ package com.estore.api.estoreapi.controller;
 
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 
 import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import com.estore.api.estoreapi.persistence.InventoryDAO;
 import com.estore.api.estoreapi.persistence.UserDAO;
+import com.estore.api.estoreapi.users.Customer;
+import com.estore.api.estoreapi.users.User;
 
 @Tag("Controller-tier")
 public class ShoppingControllerTests {
@@ -28,5 +34,38 @@ public void setupShoppingController() {
     mockUserDAO = mock(UserDAO.class);
     shoppingController = new ShoppingController(mockInventoryDAO, mockUserDAO);
 }
+
+
+@Test
+public void testCheckoutShoppingCart() throws IOException {
+
+//     //Setup
+//    User user = new User("name");
+
+//    //when the username is passed in, our mock User DAO will return the User object 
+//    when(mockUserDAO.GetUser(user.getUsername())).thenReturn(user);
+
+//    //Invoke 
+//    ResponseEntity<User> response = Shopping
+
+}
+
+@Test 
+public void testAddToShoppingCart() throws IOException{
+
+
+}
+
+@Test 
+public void testClearShoppingCart(){
+
+}
+
+@Test
+public void testRemoveFromShoppingCart(){
+    
+}
+
+
 
 }
