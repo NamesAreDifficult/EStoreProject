bfd8d1a4a421a219f9f8ebb6725c1ef2f0716d04
jrw2936 <jrw2936@g.rit.edu>
2023-03-06 16:31:55 -0500

    Removed unnecessary imports and initial commit for UserController tests

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/UserControllerTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/UserControllerTests.java
@@ -1,9 +1,36 @@
 package com.estore.api.estoreapi.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.estore.api.estoreapi.users.User;
+import com.estore.api.estoreapi.persistence.UserDAO;
+
+@Tag("Controller-tier")
 public class UserControllerTests {
+    private UserController userController;
+    private UserDAO mockUserDAO;
 
+    @BeforeEach
+    public void setupUserController() {
+        mockUserDAO = mock(UserDAO.class);
+        userController = new UserController(mockUserDAO);
+    }
 }
