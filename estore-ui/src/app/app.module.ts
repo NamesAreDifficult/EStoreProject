af9ecd14cb83f388f9fe3f09e793c0333910e539
Brian McNulty <brianmcnulty321@gmail.com>
2023-03-16 00:41:00 -0400

    removed old references

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewUserComponent } from './new-user/new-user.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewUserComponent,
-    LoginComponent
+    LoginComponent,
+    CatalogComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
