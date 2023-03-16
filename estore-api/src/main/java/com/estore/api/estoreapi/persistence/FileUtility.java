0878ca212e913f33768e7b821374554955d25f84
Brendan Battisti <brendan.battisti@gmail.com>
2023-03-15 23:37:35 -0400

    Update FileUtility.java

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/FileUtility.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/FileUtility.java
@@ -3,8 +3,19 @@ package com.estore.api.estoreapi.persistence;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Utility Class for static file related functions
+ * 
+ * @author Brendan Battisti
+ */
 public class FileUtility {
 
+    /*
+     * Takes a file path and creates the file and it's parent directories if they do
+     * not exist
+     * 
+     * @param path the path of the file
+     */
     public static void createFileWithDirectories(String path) throws IOException {
         File file = new File(path);
 
