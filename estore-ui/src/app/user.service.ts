61093e3b8eb1f453008633aa25d5a0ebb1f5255f
Brendan Battisti <brendan.battisti@gmail.com>
2023-03-15 21:58:11 -0400

    Removed redundant code

--- estore-ui/src/app/user.service.ts
+++ estore-ui/src/app/user.service.ts
@@ -53,9 +53,6 @@ export class UserService {
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
       // TODO: better job of transforming error for user consumption
       this.logger.log(`${operation} failed: ${error.message}`);
 
