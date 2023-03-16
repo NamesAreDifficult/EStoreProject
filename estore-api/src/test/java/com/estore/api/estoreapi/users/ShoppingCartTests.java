b60ec64ec873005562f2cdc01022ed55f1e81a5b
Brendan Battisti <brendan.battisti@gmail.com>
2023-03-16 11:22:30 -0400

    Fixing broken tests from new changes

--- estore-api/src/test/java/com/estore/api/estoreapi/users/ShoppingCartTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/users/ShoppingCartTests.java
@@ -8,7 +8,95 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import com.estore.api.estoreapi.products.CartBeef;
+
 @Tag("Model-tier")
 public class ShoppingCartTests {
+
+    @Test
+    public void testGetContents() {
+        // Dummy shopping cart
+        CartBeef[] list = new CartBeef[2];
+        list[0] = new CartBeef(1, 1);
+        list[1] = new CartBeef(2, 1);
+
+        ShoppingCart cart = new ShoppingCart(list);
+
+        assertArrayEquals(list, cart.getContents());
+
+    }
+
+    @Test
+    public void testAddToCart() {
+        // Dummy shopping cart
+        CartBeef[] shoppingCart = new CartBeef[2];
+        shoppingCart[0] = new CartBeef(1, 1);
+        shoppingCart[1] = new CartBeef(2, 2);
+
+        Customer testCustomer = new Customer("Test", new CartBeef[0]);
+
+        // Asserts
+        assertTrue(testCustomer.addToCart(new CartBeef(1, 1)), "Add to cart should be true");
+        assertTrue(testCustomer.addToCart(new CartBeef(2, 1)), "Add to cart should be true");
+        assertArrayEquals(shoppingCart, testCustomer.getCart());
+
+    }
+
+    @Test
+    public void testFailAddToCart() {
+        CartBeef[] shoppingCart = new CartBeef[1];
+
+        shoppingCart[0] = new CartBeef(1, 1);
+
+        Customer testCustomer = new Customer("Test", new CartBeef[0]);
+
+        // Asserts
+        assertTrue(testCustomer.addToCart(new CartBeef(1, 1)), "Add to cart should be true");
+
+        // Checks that you cannot add a duplicate beef
+        assertFalse(testCustomer.addToCart(new CartBeef(1, 2)), "Add to cart should be true");
+
+        assertArrayEquals(shoppingCart, testCustomer.getCart());
+
+    }
+
+    @Test
+    public void testClearCart() {
+
+        // Dummy shopping cart
+        CartBeef[] shoppingCart = new CartBeef[1];
+        shoppingCart[0] = new CartBeef(1, 1);
+
+        Customer testCustomer = new Customer("Test", shoppingCart);
+
+        testCustomer.clearCart();
+
+        assertArrayEquals(new CartBeef[0], testCustomer.getCart());
+
+    }
+
+    @Test
+    public void testRemoveFromCart() {
+        CartBeef[] shoppingCart = new CartBeef[2];
+
+        shoppingCart[0] = new CartBeef(1, 1);
+        shoppingCart[1] = new CartBeef(2, 1);
+
+        CartBeef[] removedShoppingCart = new CartBeef[1];
+
+        removedShoppingCart[0] = new CartBeef(2, 1);
+
+        Customer testCustomer = new Customer("Test", shoppingCart);
+
+        // Asserts
+        assertTrue(testCustomer.removeFromCart(1));
+
+        // Tests removing a CartBeef that cannot be found
+        assertFalse(testCustomer.removeFromCart(1));
+
+        // Tests that the shopping cart looks as it should
+        assertArrayEquals(removedShoppingCart, testCustomer.getCart());
+
+    }
 
 }
