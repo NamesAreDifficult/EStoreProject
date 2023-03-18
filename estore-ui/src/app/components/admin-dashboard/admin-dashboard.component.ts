b7acb4fb4ec48e9f2ede5059c634b6892eb7da48
jrw2936 <jrw2936@g.rit.edu>
2023-03-18 16:48:07 -0400

    Created method to handle status updates on press

--- estore-ui/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ estore-ui/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BeefService } from 'src/app/services/beefService/beef.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
+  beefService: BeefService;
+  adminAlert: String = ""
 
+  constructor(beefService: BeefService) {
+    this.beefService = beefService;
+  }
+
+  private catchStatusCode(code: number) {
+    // Conflict error
+    if (code == 409) {
+      this.adminAlert = "Item already exists. Please edit quantity to change amount."
+    } else if (code == 400) {
+      this.adminAlert = "Invalid attributes. If removing an item, please click delete instead."
+    } else if (code == 200) {
+      this.adminAlert = ""
+    } else if (code == 404) {
+      this.adminAlert = "Item not found."
+    } else if (code == 500) {
+      this.adminAlert = "Internal server error"
+    }
+
+  }
 }
