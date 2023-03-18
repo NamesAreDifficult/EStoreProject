99e45efdd4763a5984d35d2c3e6d344701ed012b
jrw2936 <jrw2936@g.rit.edu>
2023-03-18 16:25:21 -0400

    Added admin dashboard to routes

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: "new-user", component: NewUserComponent },
   { path: "login", component: LoginComponent },
-  { path: "catalog", component: CatalogComponent }
+  { path: "catalog", component: CatalogComponent },
+  { path: "admin-dashboard", component: AdminDashboardComponent}
 ];
 
 @NgModule({
