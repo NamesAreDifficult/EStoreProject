737d5df1095d95fa85298915a2de4a32442ba1f4
Brendan Battisti <brendan.battisti@gmail.com>
2023-03-19 19:35:58 -0400

    Fixed shopping cart tests

--- estore-api/src/test/java/com/estore/api/estoreapi/users/ShoppingCartTests.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/users/ShoppingCartTests.java
@@ -1,10 +1,10 @@
 package com.estore.api.estoreapi.users;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -13,89 +13,73 @@ import com.estore.api.estoreapi.products.CartBeef;
 @Tag("Model-tier")
 public class ShoppingCartTests {
 
+    ShoppingCart testCart;
+    CartBeef[] cartBeefs;
+    CartBeef[] clearList;
+
+    @BeforeEach
+    public void prep() {
+        cartBeefs = new CartBeef[2];
+        cartBeefs[0] = new CartBeef(1, 1);
+        cartBeefs[1] = new CartBeef(2, 1);
+
+        clearList = new CartBeef[0];
+
+        testCart = new ShoppingCart(cartBeefs);
+
+    }
+
     @Test
     public void testGetContents() {
-        // Dummy shopping cart
-        CartBeef[] list = new CartBeef[2];
-        list[0] = new CartBeef(1, 1);
-        list[1] = new CartBeef(2, 1);
 
-        ShoppingCart cart = new ShoppingCart(list);
-
-        assertArrayEquals(list, cart.getContents());
+        assertArrayEquals(cartBeefs, testCart.getContents());
 
     }
 
     @Test
     public void testAddToCart() {
-        // Dummy shopping cart
-        CartBeef[] shoppingCart = new CartBeef[2];
-        shoppingCart[0] = new CartBeef(1, 1);
-        shoppingCart[1] = new CartBeef(2, 2);
 
-        Customer testCustomer = new Customer("Test", new CartBeef[0]);
+        ShoppingCart newTestCart = new ShoppingCart();
 
         // Asserts
-        assertTrue(testCustomer.addToCart(new CartBeef(1, 1)), "Add to cart should be true");
-        assertTrue(testCustomer.addToCart(new CartBeef(2, 1)), "Add to cart should be true");
-        assertArrayEquals(shoppingCart, testCustomer.getCart());
+        assertTrue(newTestCart.addToCart(new CartBeef(1, 1)), "Add to cart should be true");
+        assertTrue(newTestCart.addToCart(new CartBeef(2, 1)), "Add to cart should be true");
+        assertArrayEquals(cartBeefs, testCart.getContents());
 
     }
 
     @Test
     public void testFailAddToCart() {
-        CartBeef[] shoppingCart = new CartBeef[1];
-
-        shoppingCart[0] = new CartBeef(1, 1);
-
-        Customer testCustomer = new Customer("Test", new CartBeef[0]);
-
-        // Asserts
-        assertTrue(testCustomer.addToCart(new CartBeef(1, 1)), "Add to cart should be true");
 
         // Checks that you cannot add a duplicate beef
-        assertFalse(testCustomer.addToCart(new CartBeef(1, 2)), "Add to cart should be true");
+        assertFalse(testCart.addToCart(new CartBeef(1, 2)), "Add to cart should be true");
 
-        assertArrayEquals(shoppingCart, testCustomer.getCart());
+        assertArrayEquals(cartBeefs, testCart.getContents());
 
     }
 
     @Test
     public void testClearCart() {
 
-        // Dummy shopping cart
-        CartBeef[] shoppingCart = new CartBeef[1];
-        shoppingCart[0] = new CartBeef(1, 1);
+        testCart.clearCart();
 
-        Customer testCustomer = new Customer("Test", shoppingCart);
-
-        testCustomer.clearCart();
-
-        assertArrayEquals(new CartBeef[0], testCustomer.getCart());
+        assertArrayEquals(clearList, testCart.getContents());
 
     }
 
     @Test
     public void testRemoveFromCart() {
-        CartBeef[] shoppingCart = new CartBeef[2];
-
-        shoppingCart[0] = new CartBeef(1, 1);
-        shoppingCart[1] = new CartBeef(2, 1);
-
-        CartBeef[] removedShoppingCart = new CartBeef[1];
-
-        removedShoppingCart[0] = new CartBeef(2, 1);
-
-        Customer testCustomer = new Customer("Test", shoppingCart);
 
         // Asserts
-        assertTrue(testCustomer.removeFromCart(1));
+        assertTrue(testCart.removeFromCart(1));
+
+        assertTrue(testCart.removeFromCart(2));
 
         // Tests removing a CartBeef that cannot be found
-        assertFalse(testCustomer.removeFromCart(1));
+        assertFalse(testCart.removeFromCart(1));
 
         // Tests that the shopping cart looks as it should
-        assertArrayEquals(removedShoppingCart, testCustomer.getCart());
+        assertArrayEquals(clearList, testCart.getContents());
 
     }
 
