a28cd89ef53e37b793cd54d400957fa4afb96727
jrw2936 <jrw2936@g.rit.edu>
2023-03-19 10:44:18 -0400

    Added documentation

--- estore-ui/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ estore-ui/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -9,41 +9,50 @@ import { Beef, BeefService } from 'src/app/services/beefService/beef.service';
 })
 
 export class AdminDashboardComponent {
-  beefService: BeefService;
-  beef$!: Observable<Beef[]>;
+  // Stores instance of BeefService class
+  beefService: BeefService
+  // Observable used to update inventory on page
+  beef$!: Observable<Beef[]>
+  // Alert for displaying success/error messages
   adminAlert: String = ""
 
+  // Constructor for Admin Dashboard that takes an instance of BeefService
   constructor(beefService: BeefService) {
-    this.beefService = beefService;
+    this.beefService = beefService
   }
 
+  // Initializes the inventory
   ngOnInit(){
-    this.beef$ = this.beefService.getAllBeef();
+    this.beef$ = this.beefService.getAllBeef()
   }
   
+  // Observer for handling creation of new beef 
   createObserver = {
     next: (beef: Beef) => {
-      this.beefService.addBeef(beef);
+      this.beefService.addBeef(beef)
     },
     error: (err: Error) => (this.createStatus(Number(err.message))),
     complete: () => this.beef$ = this.beefService.getAllBeef(),
   }
 
+  // Observer for handling deletion of beef 
   deleteObserver = {
     next: (beef: Beef) => {
-      this.beefService.deleteBeef(beef.id!);
+      this.beefService.deleteBeef(beef.id!)
     },
     error: (err: Error) => (this.deleteStatus(Number(err.message))),
     complete: () => this.beef$ = this.beefService.getAllBeef(),
   }
 
+  // Observer for handling updating of beef fields
   updateObserver = {
     next: (beef: Beef) => {
-      this.beefService.updateBeef(beef);
+      this.beefService.updateBeef(beef)
     },
     error: (err: Error) => (this.updateStatus(Number(err.message))),
   }
 
+  // Catches error code for create method and displays text
   private createStatus(code: number) {
     // Conflict error
     if (code == 409) {
@@ -57,6 +66,7 @@ export class AdminDashboardComponent {
     }
   }
 
+  // Catches error code for delete method and displays text
   private deleteStatus(code: number) {
     // Item not found error
     if (code == 404) {
@@ -67,6 +77,7 @@ export class AdminDashboardComponent {
     }
   }
 
+  // Catches error code for update method and displays text
   private updateStatus(code: number) {
     // Item not found error
     if (code == 404) {
@@ -79,26 +90,29 @@ export class AdminDashboardComponent {
     }
   }
 
-  validateCreate(cut: string, weight: number, grade: string, price: number): boolean {
-    var cutRegexp = new RegExp('^[a-zA-Z0-9 ]{1,32}$');
-    var gradeRegexp = new RegExp('^[a-zA-Z0-9 ]{1,32}$');
+  // Validates beef if cut and grade are alphanumeric with 1-32 characters and at least one non-whitespace character
+  validateCreate(cut: string, grade: string): boolean {
+    var cutRegexp = new RegExp('^[a-zA-Z0-9 ]{1,32}$')
+    var gradeRegexp = new RegExp('^[a-zA-Z0-9 ]{1,32}$')
     if ((cutRegexp.test(cut) && cut.trim()) && (gradeRegexp.test(grade) && grade.trim())){
-      return true;
+      return true
     }
     this.adminAlert = "Cut and grade must be alphanumeric with least one non-whitespace character, and have a length of 1-32 characters."
-    return false;
+    return false
   }
 
+  // Validates beef if price and weight are non-negative numbers
   validateUpdate(beef: Beef, weight: number, price: number): boolean {
     if (beef.weight + weight >= 0 && price >= 0){
-      return true;
+      return true
     }
     this.adminAlert = "Items cannot have negative weight or price."
-    return false;
+    return false
   }
 
+  // Adds new beef object to inventory if the fields are valid
   create(cut: string, weight: number, grade: string, price: number) {
-    if (this.validateCreate(cut, weight, grade, price)){
+    if (this.validateCreate(cut, grade)){
       var beef: Beef = {
         cut: cut.trim(),
         weight: weight,
@@ -110,15 +124,17 @@ export class AdminDashboardComponent {
     }
   }
 
+  // Deletes beef object from the inventory
   delete(id: number) {
     this.beefService.deleteBeef(id).subscribe(this.deleteObserver);
     this.adminAlert = "Product deleted."
   }
 
+  // Updates price and weight of beef objects if the fields are valid
   update(beef: Beef, weight: number, price: number) {
       if (this.validateUpdate(beef, weight, price)){
-      beef.weight += weight;
-      beef.price = price;
+      beef.weight += weight
+      beef.price = price
       this.beefService.updateBeef(beef).subscribe(this.updateObserver);
       this.adminAlert = "Product updated."
     }
