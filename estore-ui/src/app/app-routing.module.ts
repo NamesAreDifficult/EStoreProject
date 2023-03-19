61ab2ea4d7dba129b69dcb2476b95b0f01be9752
ajc2732 <ajc2732@rit.edu>
2023-03-19 16:45:15 -0400

    added routing to app routing module

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
-
+import { HomePageComponent } from './components/home-page/home-page.component';
 const routes: Routes = [
   { path: "new-user", component: NewUserComponent },
   { path: "login", component: LoginComponent },
-  { path: "catalog", component: CatalogComponent }
+  { path: "catalog", component: CatalogComponent },
+  { path: "home page", component: HomePageComponent}
 ];
 
 @NgModule({
