29b3757fa822466e53ad0965d2a5f4880b9736cb
Brian McNulty <brianmcnulty321@gmail.com>
2023-03-19 20:22:27 -0400

    Added comments, removed threaded funciton

--- estore-ui/src/app/components/catalog/catalog.component.ts
+++ estore-ui/src/app/components/catalog/catalog.component.ts
@@ -13,20 +13,16 @@ export class CatalogComponent {
   beef$!: Observable<Beef[]>;
   private searchTerms = new BehaviorSubject<string>('');
 
-  constructor(private beefService: BeefService) {
-  }
+  constructor(private beefService: BeefService) {}
 
+  // Adds a new term to the behaviorsubject for searching
   search(term: string): void{
     this.searchTerms.next(term);
   }
 
+  // sets up the beef observable to to update with the search terms
   ngOnInit(): void{
     // The pipe operations cause a 300 ms delay between keystrokes, waits until the input has changed for the next search and remaps the observable once the search is completed
-    this.searchWatcher();
-  }
-
-
-  searchWatcher = async(): Promise<void> => {
     this.beef$ = this.searchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
