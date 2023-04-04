9dc0a52071ea6f7ec11367528129729eff68b51a
Groovymeister <jrw2936@rit.edu>
2023-04-04 09:05:35 -0400

    Created credit card class

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/users/CreditCard.java
@@ -0,0 +1,33 @@
+package com.estore.api.estoreapi.users;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/*
+ * Represents a Credit Card
+ *  
+ * @author John West
+ */
+
+public class CreditCard {
+
+    @JsonProperty("number")
+    private String number;
+
+    @JsonProperty("expiration")
+    private String expiration;
+
+    @JsonProperty("cvv")
+    private String cvv;
+
+    /*
+     * Constructs a credit card using the card number, expiration date, and cvv
+     */
+    public CreditCard(@JsonProperty("number") String number,
+                      @JsonProperty("expiration") String expiration,
+                      @JsonProperty("cvv") String cvv){
+        
+        this.number = number;
+        this.expiration = expiration;
+        this.cvv = cvv;
+    }
+}
