ee5e9e375cdcb847717fcf2e04087ec315f49b05
Brendan Battisti <brendan.battisti@gmail.com>
2023-04-03 23:10:42 -0400

    Revert "Created a base template component and changed navbar conditionals"
    This reverts commit 41ad2fbbfafa2e2b49be2a92714081f0834e9350.

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -19,7 +19,6 @@ import { LogoutComponent } from './components/logout/logout.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { FormsModule } from '@angular/forms';
-import { BaseTemplateComponent } from './base-template/base-template.component';
 
 
 @NgModule({
@@ -33,8 +32,7 @@ import { BaseTemplateComponent } from './base-template/base-template.component';
     AdminDashboardComponent,
     LogoutComponent,
     CartComponent,
-    ProductDetailsComponent,
-    BaseTemplateComponent
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
