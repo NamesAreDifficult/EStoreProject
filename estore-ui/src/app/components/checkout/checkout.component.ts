17df1a9a38b8356e565baa31d49780c28c143910
Brendan Battisti <brendan.battisti@gmail.com>
2023-04-06 05:40:07 -0400

    Working on sending user to account page

--- estore-ui/src/app/components/checkout/checkout.component.ts
+++ estore-ui/src/app/components/checkout/checkout.component.ts
@@ -5,7 +5,7 @@ import { Beef } from 'src/app/services/beefService/beef.service';
 import { LoggingService } from 'src/app/services/loggingService/logging.service';
 import { CardService, CreditCard } from 'src/app/services/cardService/card.service';
 import { User, UserService } from 'src/app/services/userService/user.service';
-
+import { isEmpty } from 'rxjs/operators';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -15,49 +15,51 @@ export class CheckoutComponent {
   checkoutAlert = ""
   cart$!: Observable<Beef[]>
   isEmpty!: boolean;
+  cardsEmpty!: boolean;
   cards$!: Observable<CreditCard[]>
   currentCard: CreditCard | undefined;
   user!: User | null;
   total: number = 0
   constructor(private shoppingService: CartServiceService, private userService: UserService,
-              private cardService: CardService) {
+    private cardService: CardService) {
   }
 
   ngOnInit() {
     this.cards$ = this.cardService.getCards()
+    this.cards$.pipe(isEmpty()).subscribe(val => this.cardsEmpty = val);
     this.cart$ = this.shoppingService.getCart().pipe(
-      tap((cartItems: Beef[]) =>{
+      tap((cartItems: Beef[]) => {
         this.isEmpty = cartItems.length === 0;
       })
     );
     this.cart$.subscribe(cart => {
       this.total = 0
-      for (var item of cart){
+      for (var item of cart) {
         this.total += (item.price * item.weight)
       }
     })
     this.userService.userNotifier.subscribe(currentUser => {
       this.user = currentUser;
-      if(currentUser == null){
+      if (currentUser == null) {
         location.reload()
       }
     });
   }
 
-  selectCard(card: CreditCard){
+  selectCard(card: CreditCard) {
     this.currentCard = card
   }
 
-  checkout(){
-    if (this.currentCard === undefined){
+  checkout() {
+    if (this.currentCard === undefined) {
       this.checkoutAlert = "Please select a credit card"
     }
-    else{
-    this.shoppingService.checkout(this.currentCard.number).subscribe(
-      {
-        next: (any: any) => this.cart$ = this.shoppingService.checkout(this.currentCard!.number)
-      }
-    )
+    else {
+      this.shoppingService.checkout(this.currentCard.number).subscribe(
+        {
+          next: (any: any) => this.cart$ = this.shoppingService.checkout(this.currentCard!.number)
+        }
+      )
       document.getElementById("checkoutButton")!.style.visibility = "hidden";
       this.checkoutAlert = "Thanks for shopping at Cow-Related Pun!"
     }
